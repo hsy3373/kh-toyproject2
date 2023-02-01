3a803f7abf6f76df1368e311819db2fd0c72804f
hasusungbin <117426079+hasusungbin@users.noreply.github.com>
2023-02-01 14:05:31 +0900

    han-imgList 이미지불러오기 50%완 02.01

--- han/imgList.js
+++ han/imgList.js
@@ -25,6 +25,8 @@ let loadedImgAni = function (selector) {
 // ---------------------picsum---------------------------------
 
 /*
+https://picsum.photos/
+
 picsum 객체 예시 
 {
   author: "Alejandro Escamilla"
@@ -46,13 +48,26 @@ let getImgesPicsum = function (page, number) {
     // ajax가 정보를 요청하기 전단계에서 실행됨
     beforeSend: function () {
       $(".loadingAni").fadeIn(300);
-      // todo! 로딩 애니메이션 넣기
     },
   }).done(function (msg) {
     // 모든 통신이 끝난 후 동작하며 msg에 위에서 지정한 json 타입으로 정보가 들어옴
     // picsum용 이미지 세팅 실행
-    console.log(msg);
     putImgPicsum(msg);
+  });
+};
+
+let getOneImgePicsum = function (id) {
+  $.ajax({
+    method: "GET",
+    url: `https://picsum.photos/id/${id}/info`,
+    dataType: "json",
+    // ajax가 정보를 요청하기 전단계에서 실행됨
+    beforeSend: function () {
+      $(".loadingAni").fadeIn(300);
+    },
+  }).done(function (msg) {
+    // todo! : 페이지 이동 및 정보 넘기기처리하기
+    console.log(typeof msg, msg);
   });
 };
 
@@ -67,7 +82,7 @@ let putImgPicsum = function (msg) {
     }
 
     $(`.imgListWrap>:nth-child(${int})`).append(
-      `<div class="loadedImgDiv"><img class="loadedImg picsum" src="${item.download_url}" id="${item.id}"></img></div>`
+      `<div class="loadedImgDiv"><img class="loadedImg picsum" src="${item.download_url}" id="${item.id}"></img><div class="favoriteDiv"></div></div>`
     );
     // 추후 픽섬에서 해당 이미지 아이디값을 검색해와야 할 경우를 위해 따로 클래스명 둠
   }
@@ -80,7 +95,6 @@ let putImgPicsum = function (msg) {
 };
 
 // ---------------------unsplash---------------------------------
-
 /*
 Unsplash 객체 예시
 {
@@ -121,21 +135,23 @@ Unsplash 객체 예시
 
 // todo! : 권한 유효성 이슈로 이미지 끌어오지 못하고 있음 해결필수!
 let getImgesUnsplash = function (type, count) {
+  console.log("getImgesUnsplash 시작");
   $.ajax({
     method: "GET",
-    url: `https://api.unsplash.com/photos/${type}?count=${count}`,
+    url: `https://api.unsplash.com/photos/random?count=15`,
     dataType: "json",
     beforeSend: function (xhr) {
-      $(".loadingAni").fadeIn(300);
+      $("#loading").show();
       xhr.setRequestHeader(
         "Authorization",
         "Client-ID " + "8f8MJ6PUF8d_TQPhyNFoow_r8dZXUXojqWuT1oBEw5k"
       );
     },
   }).done(function (msg) {
-    console.log(msg);
+    console.log("getImgesUnsplash 끝");
     putImgUnsplash(msg);
   });
+  console.log("끝");
 };
 
 let putImgUnsplash = function (msg) {
@@ -148,17 +164,37 @@ let putImgUnsplash = function (msg) {
     }
 
     $(`.imgListWrap>:nth-child(${int})`).append(
-      `<div class="loadedImgDiv"><img class="loadedImg unsplash" src="${item.urls.regular}" id="${item.id}"></img></div>`
+      `<div class="loadedImgDiv"><img class="loadedImg unsplash" src="${item.urls.regular}" id="${item.id}"></img><div class="favoriteDiv"></div></div>`
     );
   }
 
-  $(`img[src="${$(msg).get(-1).download_url}"]`).on(
+  $(`img[src="${$(msg).get(-1).urls.regular}"]`).on(
     `load`,
     loadedImgAni(".loadedImg")
   );
 };
 
-// --------------------------스크롤 최하단 시 동작----------------------
+// ---------------------이미지 개별 클릭 이벤트--------------
+
+// 이미지 클릭 시 아이디 값 가져와서 해당 사이트에서 아이디값으로 검색 가능함
+// todo! 검색된 정보를 어디서 불러올지 정해서 구현하기
+$(document).on("click", ".picsum", function () {
+  getOneImgePicsum($(this).attr("id"));
+});
+
+$(document).on("click", ".favoriteDiv", function () {
+  // todo! 즐겨찾기 추가
+  console.log("즐겨찾기 추가");
+});
+
+$(document).on("mouseover", ".loadedImgDiv", function (e) {
+  $(this).children("div").css("opacity", 1);
+});
+$(document).on("mouseleave", ".loadedImgDiv", function (e) {
+  $(this).children("div").css("opacity", 0);
+});
+
+// --------------------------페이지 기본 동작들 구역----------------------
 
 // 스크롤 동작에 따라 이미지를 불러오기 위한 페이지 변수
 var page = 1;
@@ -168,13 +204,12 @@ let init = function () {
   // getImgesUnsplash("random", 15);
   getImgesPicsum(page, $(".imgListWrap").children().length * 6);
 
-  // 스크롤이 최 하단에 닿을때마다 페이지 수 증가시키며 이미지 불러오기
+  // 스크롤이 하단에 닿을때마다 페이지 수 증가시키며 이미지 불러오기
   $(window).scroll(function () {
     if ($(window).scrollTop() == $(document).height() - $(window).height()) {
       console.log(++page);
       // getImgesUnsplash("random", 15);
       getImgesPicsum(page, 15);
-      console.log("무한스크롤: " + page);
     }
   });
 };
@@ -186,9 +221,45 @@ $(window).on("load", () => {
     //  <-* 로딩속도 구현
     // $(".loadingAni").hide(300);
     $("html").css({ overflow: "auto" }); //스크롤 방지 해제
-    console.log("페이지 로드 완료");
 
     // 윈도우가 모두 로드된 후 기본 메소드들을 실행시킨다
     init();
   }, 2000); //  <-* 로딩속도 구현
 });
+
+// ---------------- 로컬 스토리지와 연동 구역 -------------------
+
+// user 객체
+
+// let userList = {
+//   userId1: {
+//     id: "userId1",
+//     pwd: "pwd1",
+//     favorite: [
+//       { type: "picsum", id: "id" },
+//       { type: "picsum", id: "id" },
+//       { type: "unsplash", id: "id" },
+//     ],
+//   },
+//   userId2: {},
+// };
+// let currentUser = "userId";
+
+// ----------------------
+/*
+
+// 키에 데이터 쓰기
+localStorage.setItem("key", value);
+
+// 키로 부터 데이터 읽기
+localStorage.getItem("key");
+
+// 키의 데이터 삭제
+localStorage.removeItem("key");
+
+// 모든 키의 데이터 삭제
+localStorage.clear();
+
+// 저장된 키/값 쌍의 개수
+localStorage.length
+*/
