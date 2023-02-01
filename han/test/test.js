8150d8289e069173fa11e7842f3cf1262c12ee10
hsy3373 <hsy3373@naver.com>
2023-02-01 17:33:19 +0900

    han-02.01

---
+++ han/test/test.js
@@ -0,0 +1,11 @@
+// const myModal = document.getElementById("#exampleModal");
+
+// myModal.addEventListener("shown.bs.modal", () => {});
+
+// $(function () {
+//   $("button").click(function () {
+//     $("#exampleModal").on("shown.bs.modal", function () {});
+//   });
+// });
+
+AOS.init();
